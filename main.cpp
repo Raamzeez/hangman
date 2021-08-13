1f25da1a9e868531a2309a6ff45ec4daa0561803
Mohammed Abbasi <raamizabbasi@Mohammeds-MacBook-Pro.local>
2021-08-13 14:06:54 -0400

    Practically completed. Error handling could be added later

--- main.cpp
+++ main.cpp
@@ -10,21 +10,34 @@ struct evaluateGuessOuput
     vector<int> indexVector;
 };
 
-void printGuessingText(string text)
+bool victory(string guessingText)
+{
+    bool victory = true;
+    for (int i = 0; i < guessingText.length(); i++)
+    {
+        if (guessingText[i] == '_')
+        {
+            victory = false;
+        }
+    }
+    return victory;
+}
+
+string generateGuessingText(string originalText)
 {
     string guessingText;
-    for (int i = 0; i < text.length(); i++)
+    for (int i = 0; i < originalText.length(); i++)
     {
-        if (text[i] == ' ')
+        if (originalText[i] == ' ')
         {
-            guessingText.push_back(text[i]);
+            guessingText.push_back(originalText[i]);
         }
         else
         {
             guessingText.push_back('_');
         }
     }
-    cout << guessingText << endl;
+    return guessingText;
 }
 
 evaluateGuessOuput evaluateGuess(char letterGuessed, string text)
@@ -44,30 +57,80 @@ evaluateGuessOuput evaluateGuess(char letterGuessed, string text)
 
 int main()
 {
+    bool win = false;
     bool gameRunning = true;
+    bool askForPhrase = true;
     int currentStageIndex = 0;
     vector<char> guessedCharacters;
     string targetPhrase;
+    char letterGuessed;
+    string guessingText;
     while (gameRunning)
     {
-        char letterGuessed;
-        cout << "Please type the word you want to guess " << endl;
-        getline(cin, targetPhrase);
-        printGuessingText(targetPhrase);
-        cout << "What letter do you want to guess? ";
-        if (cin >> letterGuessed)
+        if (askForPhrase)
         {
+            cout << "Please type the word you want to guess " << endl;
+            getline(cin, targetPhrase);
+            // if (cin.fail())
+            // {
+            //     cout << "You typed an incorrect input. Please try again. " << endl;
+            //     continue;
+            // }
+            // else
+            // {
+            guessingText = generateGuessingText(targetPhrase);
+            askForPhrase = false;
+            // }
+        }
+        if (currentStageIndex == 6)
+        {
+            cout << stages[currentStageIndex] << endl;
+            gameRunning = false;
+            break;
+        }
+        if (victory(guessingText))
+        {
+            win = true;
+            gameRunning = false;
+            break;
+        }
+        cout << stages[currentStageIndex] << endl;
+        cout << guessingText << endl;
+        cout << "What letter do you want to guess? ";
+        cin >> letterGuessed;
+        if (!cin.fail())
+        {
+            guessedCharacters.push_back(letterGuessed);
             evaluateGuessOuput data = evaluateGuess(letterGuessed, targetPhrase);
-            for (int i = 0; i < data.indexVector.size(); i++)
+            if (data.successfulGuess)
             {
-                
+                for (int i = 0; i < data.indexVector.size(); i++)
+                {
+                    guessingText[data.indexVector.at(i)] = letterGuessed;
+                }
+                cout << "You guessed the correct letter " << letterGuessed << endl;
             }
+            else
+            {
+                cout << "You guessed an incorrect letter" << endl;
+                currentStageIndex++;
+            }
+            continue;
         }
         else
         {
+            cin.clear();
             cout << "Incorrect input. Please try again. " << endl;
             continue;
         }
+    }
+    if (win)
+    {
+        cout << "Congratulations you won the game!" << endl;
+    }
+    else
+    {
+        cout << "Sorry, you lost the game! The correct phrase was " << targetPhrase << endl;
     }
     return 0;
 }
